fb282fe2b1d0ba876288ec76a079ee4a4873c50e
linsir <student_yxl@126.com>
2026-01-19 20:20:05 +0800

    门户vue 版本迁移

---
+++ linsir-linrk/nuxt.config.js
@@ -0,0 +1,44 @@
+export default {
+  head: {
+    title: 'Linsir',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap',
+      },
+    ],
+  },
+  css: ['@/assets/css/main.css', 'aos/dist/aos.css'],
+  plugins: ['~/plugins/smooth-scroll.js', '~/plugins/mdi.js','~/plugins/axios'],
+  components: true,
+  buildModules: ['@nuxtjs/eslint-module', '@nuxt/postcss8','@nuxtjs/dotenv'],
+  modules: ['@nuxtjs/axios'],
+  axios: {
+    baseURL: 'http://localhost:9000', // Used as fallback if no runtime config is provided
+    proxy: true,
+  },
+  proxy: {
+    '/api/': 'http://localhost:9000',
+  },
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  },
+  /**
+  buildDir: 'nuxt-dist',
+   */
+}
